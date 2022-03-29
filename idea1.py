51b235da7bf2f726103122ffc12ad3468c7197e5
MuhtasimArko <102625926+MuhtasimArko@users.noreply.github.com>
2022-03-30 02:30:37 +0600

    Add files via upload

---
+++ idea1.py
@@ -0,0 +1,11 @@
+taka=float(input("Taka:"))
+dollar=float(input("Dollar:"))
+while taka:
+    c=taka/85
+    print(taka,"TAKA = ",c,"DOLLAR")
+    break
+while dollar:
+    c=dollar*85
+    print(dollar,"DOLLAR = ",c,"TAKA")
+    break
+
